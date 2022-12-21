8f46059f050fb795c77dc3836d1a5a05ec2c92b0
Bhavanaraj2000 <109426104+Bhavanaraj2000@users.noreply.github.com>
2022-12-21 11:55:22 +0530

    Add files via upload

---
+++ heightfl.js
@@ -0,0 +1,13 @@
+
+        function converter(){
+            var feet=document.getElementById('num').value
+            var inches=feet*12;
+            console.log(inches);
+            document.getElementById('num1').value=inches
+
+
+
+        }
+        
+
+    
